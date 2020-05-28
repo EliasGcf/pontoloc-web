8569398199691976307a771f24373c1e53a79725
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-05-28 15:03:55 -0300

    feat: add toast context and hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { AuthProvider } from './auth';
+import { ToastProvider } from './toast';
 
 const AppProvider: React.FC = ({ children }) => (
-  <AuthProvider>{children}</AuthProvider>
+  <AuthProvider>
+    <ToastProvider>{children}</ToastProvider>
+  </AuthProvider>
 );
 
 export default AppProvider;
